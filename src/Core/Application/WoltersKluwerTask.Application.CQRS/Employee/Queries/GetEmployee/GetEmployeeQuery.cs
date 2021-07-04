44c16390468f54b97d5419ea379d63f1281bda30
Pawel Sulzycki <sulzyckipawel@gmail.com>
2021-07-04 17:04:17 +0200

    Create GetEmployeeQueryHandler

---
+++ src/Core/Application/WoltersKluwerTask.Application.CQRS/Employee/Queries/GetEmployee/GetEmployeeQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using WoltersKluwerTask.Domain.ValueObjects;
+
+namespace WoltersKluwerTask.Application.CQRS.Employee.Queries.GetEmployee
+{
+    public class GetEmployeeQuery : IRequest<Domain.Entities.Employee>
+    {
+        public EmployeeId EmployeeId { get; set; }
+    }
+}
---
+++ src/Core/Application/WoltersKluwerTask.Application.CQRS/Employee/Queries/GetEmployee/GetEmployeeQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+using WoltersKluwerTask.Application.Contracts.Repositories;
+
+namespace WoltersKluwerTask.Application.CQRS.Employee.Queries.GetEmployee
+{
+    public class GetEmployeeQueryHandler : IRequestHandler<GetEmployeeQuery, Domain.Entities.Employee>
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public GetEmployeeQueryHandler(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        public async Task<Domain.Entities.Employee> Handle(GetEmployeeQuery request, CancellationToken cancellationToken)
+        {
+            var employee = await _employeeRepository.GetByIdAsync(request.EmployeeId.Value);
+
+            return employee;
+        }
+    }
+}
