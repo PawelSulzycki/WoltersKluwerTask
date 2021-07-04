d49ea37c1b07d4ae60ed886a075bfe2c578e55a2
Pawel Sulzycki <sulzyckipawel@gmail.com>
2021-07-04 12:17:29 +0200

    Create EmployeeId, ValueObject and Employee

---
+++ src/Core/Domain/WoltersKluwerTask.Domain/Entities/Employee.cs
@@ -0,0 +1,9 @@
+﻿using WoltersKluwerTask.Domain.Ddd;
+using WoltersKluwerTask.Domain.ValueObjects;
+
+namespace WoltersKluwerTask.Domain.Entities
+{
+    public class Employee : Entity<EmployeeId>
+    {
+    }
+}
---
+++ src/Core/Domain/WoltersKluwerTask.Domain/ValueObjects/EmployeeId.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using WoltersKluwerTask.Domain.Ddd;
+
+namespace WoltersKluwerTask.Domain.ValueObjects
+{
+    public class EmployeeId : ValueObject
+    {
+        public int Value { get; set; }
+
+        public EmployeeId(int value)
+        {
+            Value = value;
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Value;
+        }
+    }
+}
