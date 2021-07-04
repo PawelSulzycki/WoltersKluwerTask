a1c19cbe78f526392e181738bed1601180109562
Pawel Sulzycki <sulzyckipawel@gmail.com>
2021-07-04 17:10:21 +0200

    Create GetAllEmployeesQueryHandler

---
+++ src/Core/Application/WoltersKluwerTask.Application.CQRS/Employee/Queries/GetAllEmployees/GetAllEmployeesQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using System.Collections.Generic;
+
+namespace WoltersKluwerTask.Application.CQRS.Employee.Queries.GetAllEmployees
+{
+    public class GetAllEmployeesQuery : IRequest<IEnumerable<Domain.Entities.Employee>>
+    {
+    }
+}
---
+++ src/Core/Application/WoltersKluwerTask.Application.CQRS/Employee/Queries/GetAllEmployees/GetAllEmployeesQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using WoltersKluwerTask.Application.Contracts.Repositories;
+
+namespace WoltersKluwerTask.Application.CQRS.Employee.Queries.GetAllEmployees
+{
+    public class GetAllEmployeesQueryHandler : IRequestHandler<GetAllEmployeesQuery, IEnumerable<Domain.Entities.Employee>>
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public GetAllEmployeesQueryHandler(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        public async Task<IEnumerable<Domain.Entities.Employee>> Handle(GetAllEmployeesQuery request, CancellationToken cancellationToken)
+        {
+            var employees = await _employeeRepository.GetAllAsync();
+
+            return employees;
+        }
+    }
+}
