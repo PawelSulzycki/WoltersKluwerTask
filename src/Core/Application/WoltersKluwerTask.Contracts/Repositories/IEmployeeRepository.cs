a5638ad593674d22e06a72f9be24db52fbf65be0
Pawel Sulzycki <sulzyckipawel@gmail.com>
2021-07-04 16:29:21 +0200

    Create IAsyncRepository and IEmployeeRepository

---
+++ src/Core/Application/WoltersKluwerTask.Contracts/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,8 @@
+﻿using WoltersKluwerTask.Domain.Entities;
+
+namespace WoltersKluwerTask.Contracts.Repositories
+{
+    public interface IEmployeeRepository : IAsyncRepository<Employee>
+    {
+    }
+}
