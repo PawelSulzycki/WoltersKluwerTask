1890c4f1b15418e774d8fc4a5872355193bd4a40
Pawel Sulzycki <sulzyckipawel@gmail.com>
2021-07-04 14:12:10 +0200

    Create Name ValueObject

---
+++ src/Core/Domain/WoltersKluwerTask.Domain/ValueObjects/Name.cs
@@ -0,0 +1,24 @@
+﻿using Dawn;
+using System.Collections.Generic;
+using WoltersKluwerTask.Domain.Ddd;
+
+namespace WoltersKluwerTask.Domain.ValueObjects
+{
+    public class Name : ValueObject
+    {
+        public string First { get; private set; }
+        public string Last { get; private set; }
+
+        public Name(string first, string last)
+        {
+            First = Guard.Argument(first, nameof(first)).NotEmpty().NotNull().MaxLength(25);
+            Last = Guard.Argument(last, nameof(last)).NotEmpty().NotNull().MaxLength(50);
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return First;
+            yield return Last;
+        }
+    }
+}
