c1041380821ba3cbca2058f159d4bf06d67b0e58
Pawel Sulzycki <sulzyckipawel@gmail.com>
2021-07-04 21:12:00 +0200

    Create GetAllEmployeesTests

--- src/Core/Application/WoltersKluwerTask.Application.CQRS/Employee/Queries/GetAllEmployees/GetAllEmployeesQueryResponse.cs
+++ src/Core/Application/WoltersKluwerTask.Application.CQRS/Employee/Queries/GetAllEmployees/GetAllEmployeesQueryResponse.cs
@@ -5,7 +5,7 @@ namespace WoltersKluwerTask.Application.CQRS.Employee.Queries.GetAllEmployees
 {
     public class GetAllEmployeesQueryResponse : BaseResponse
     {
-        IEnumerable<Domain.Entities.Employee> Employees { get; set; }
+        public IEnumerable<Domain.Entities.Employee> Employees { get; set; }
 
         public GetAllEmployeesQueryResponse(IEnumerable<Domain.Entities.Employee> employees) : base()
         {
---
+++ tests/WoltersKluwerTask.UnitTests/CQRSTests/Commands/GetAllEmployeesTests.cs
@@ -0,0 +1,103 @@
+﻿using FluentAssertions;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WoltersKluwerTask.Application.Common;
+using WoltersKluwerTask.Application.Contracts.Repositories;
+using WoltersKluwerTask.Application.CQRS.Employee.Queries.GetAllEmployees;
+using WoltersKluwerTask.Domain.Entities;
+using WoltersKluwerTask.Domain.Enums;
+using WoltersKluwerTask.Domain.ValueObjects;
+using Xunit;
+
+namespace WoltersKluwerTask.UnitTests.CQRSTests.Commands
+{
+    public class GetAllEmployeesTests
+    {
+        private readonly GetAllEmployeesQueryHandler _handler;
+        private readonly Mock<IEmployeeRepository> _mockEmployeeRepository;
+
+        public GetAllEmployeesTests()
+        {
+            _mockEmployeeRepository = new Mock<IEmployeeRepository>();
+
+            _handler = new GetAllEmployeesQueryHandler(_mockEmployeeRepository.Object);
+        }
+
+        [Fact]
+        public async Task When_Return_Empty_List_From_Database_Return_Success()
+        {
+            var query = new GetAllEmployeesQuery();
+
+            _mockEmployeeRepository
+                .Setup(x => x.GetAllAsync())
+                .ReturnsAsync(new List<Employee>());
+
+            var response = await _handler.Handle(query, new System.Threading.CancellationToken());
+
+            response.Status.Should().Be(ResponseStatus.Success);
+            response.Success.Should().Be(true);
+            response.Employees.Should().BeEmpty();
+        }
+
+        [Fact]
+        public async Task When_Return_Empty_List_From_Database_Verify_Method()
+        {
+            var query = new GetAllEmployeesQuery();
+
+            _mockEmployeeRepository
+                .Setup(x => x.GetAllAsync())
+                .ReturnsAsync(new List<Employee>());
+
+            var response = await _handler.Handle(query, new System.Threading.CancellationToken());
+
+            _mockEmployeeRepository
+                .Verify(x => x.GetAllAsync(), Times.Once());
+        }
+
+        [Fact]
+        public async Task When_Return_List_From_Database_Return_Success()
+        {
+            var query = new GetAllEmployeesQuery();
+
+            var employees = new List<Employee>()
+            {
+                new Employee(new EmployeeId(1), EvidenceNumber.Create(1), new Pesel("12345678910"), DateTime.Now, new Name("TestFirst", "TestLast"), Gender.Man),
+                new Employee(new EmployeeId(2), EvidenceNumber.Create(2), new Pesel("12345678911"), DateTime.Now, new Name("TestFirst2", "TestLast2"), Gender.Woman)
+            };
+
+            _mockEmployeeRepository
+                .Setup(x => x.GetAllAsync())
+                .ReturnsAsync(employees);
+
+            var response = await _handler.Handle(query, new System.Threading.CancellationToken());
+
+            response.Status.Should().Be(ResponseStatus.Success);
+            response.Success.Should().Be(true);
+            response.Employees.Should().NotBeEmpty()
+                .And.HaveCount(employees.Count);
+        }
+
+        [Fact]
+        public async Task When_Return_List_From_Database_Verify_Method()
+        {
+            var query = new GetAllEmployeesQuery();
+
+            var employees = new List<Employee>()
+            {
+                new Employee(new EmployeeId(1), EvidenceNumber.Create(1), new Pesel("12345678910"), DateTime.Now, new Name("TestFirst", "TestLast"), Gender.Man),
+                new Employee(new EmployeeId(2), EvidenceNumber.Create(2), new Pesel("12345678911"), DateTime.Now, new Name("TestFirst2", "TestLast2"), Gender.Woman)
+            };
+
+            _mockEmployeeRepository
+                .Setup(x => x.GetAllAsync())
+                .ReturnsAsync(employees);
+
+            var response = await _handler.Handle(query, new System.Threading.CancellationToken());
+
+            _mockEmployeeRepository
+                .Verify(x => x.GetAllAsync(), Times.Once());
+        }
+    }
+}
